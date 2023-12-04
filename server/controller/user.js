45fc7bdb0e430d4e0a60509043813e4ab77dfeb6
Sangam Prajapati <prajapatisangam91@gmail.com>
2023-12-05 00:41:26 +0530

    Authentication

--- server/controller/user.js
+++ server/controller/user.js
@@ -12,17 +12,17 @@ async function handleUserSignup(req,res){
     newUser.save()
   .then(() => {
     console.log('User saved successfully!');
-    return res.status(201).send({message : 'User saved successfully!'});
+    return res.status(201).send({message : 'User saved successfully!',status:201});
     mongoose.connection.close(); // Close the connection after saving
   })
   .catch((error) => {
     console.error('Error saving user:', error);
-    return res.status(401).send({message : 'Error saving the new user'});
+    return res.status(401).send({message : 'Error saving the new user',status:401});
     mongoose.connection.close(); // Close the connection on error
   });
     } catch (error) {
         console.log("User Already Exist");
-        return res.status(401).send({message : "User Already Exist"});
+        return res.status(401).send({message : "User Already Exist",status:401});
     }
 }
 
@@ -30,7 +30,7 @@ async function handleUserLogin(req,res){
   const {email,password} = req.body;
   const user = await User.findOne({email});
   if(!user || user.password != password){
-    return res.status(401).send({message:"Wrong User"});
+    return res.status(401).send({message:"Invalid User" , status:401});
   }
   const token = createJwtToken(user);
   res.cookie("token", token);
@@ -41,6 +41,7 @@ async function handleUserLogin(req,res){
 return res.status(201).json({status:201,result});
   
 }
+
 
 module.exports = {
     handleUserSignup,
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,18 @@
 const {Router} = require('express');
-
-
+const authenticate = require('../middleware/auth');
+const User = require('../schema/user');
 const router = Router();
 const { handleUserSignup, handleUserLogin } = require("../controller/user");
 
 router.post('/signup',handleUserSignup);
 router.post("/login", handleUserLogin);
+router.get("/validuser",authenticate,async(req,res)=>{
+    try {
+        const ValidUserOne = await User.findOne({_id:req.userId});
+        res.status(201).json({status:201,ValidUserOne});
+    } catch (error) {
+        res.status(401).json({status:401,error});
+    }
+  });
 
 module.exports = router;
